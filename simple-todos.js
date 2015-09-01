026ea8630b3c3c37d60b2697cfa17937b2740bbb
Mason Crane <masonecrane@gmail.com>
2015-09-01 15:09:44 -0600

    added bootstrap, less, new template for displaying monogo data, event function to properly switch is_done property in DB

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,26 @@
-if (Meteor.isClient) {
+Todos = new Meteor.Collection('todos');
+
+if(Meteor.isClient) {
     // This code only runs on the client
-    Template.body.helpers({
-        tasks: [
-            { text: 'This is task 1' },
-            { text: 'This is task 2' },
-            { text: 'This is task 3' }
-        ]
+    Template.TodosPanel.helpers({
+        items: function() {
+            return Todos.find();
+        }
     });
+    Template.TodoItem.helpers({
+        isDoneChecked: function() {
+            return this.is_done ? 'checked' : '';
+        }
+    });
+    Template.TodoItem.events({
+        'click [name=is_done]': function(e, tmpl) {
+            var id = this._id;
+            var isDone = tmpl.find('input').checked;
+            Todos.update({_id: id}, {$set:{is_done: isDone}});
+        }
+    });
+}
+
+if(Meteor.isServer) {
+
 }
